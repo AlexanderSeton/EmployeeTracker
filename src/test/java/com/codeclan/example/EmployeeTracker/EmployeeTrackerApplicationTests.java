37a81ec0f683323e927c3d9bf71e569b8c69f463
Alexander Seton <alexanderseton@protonmail.com>
2022-01-17 13:57:53 +0000

    Added getAll and getId routes.

--- src/test/java/com/codeclan/example/EmployeeTracker/EmployeeTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/EmployeeTracker/EmployeeTrackerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.codeclan.example.EmployeeTracker;
 
+import com.codeclan.example.EmployeeTracker.models.Employee;
+import com.codeclan.example.EmployeeTracker.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeeTrackerApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void createEmployee() {
+		Employee testEmployee = new Employee("Tim", 42, 43290842, "tim@gmail.com");
+		employeeRepository.save(testEmployee);
 	}
 
 }
